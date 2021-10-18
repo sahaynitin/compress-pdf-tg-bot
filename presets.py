3ebd54048cc60c0a1dea3726a1d9d4bdb7e66d74
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-18 19:30:00 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,8 +1,7 @@
 class Presets(object):
-    WELCOME_TXT = "<b>Hello.. {}</b>\n<i>I can compress the size of pdf docs. Send me a pdf document to see " \
-                  "the magic !</i> "
-    INVALID_FORMAT = "<b>Error:</b>\n<i>I can only compress pdf documents. Please make it sure, you have given me a " \
-                     "valid document. Try again...</i>"
+    WELCOME_TXT = "<b>Hey {}</b>\n\n<b>I can compress the size of pdf files.\n\n Just Send me a pdf File to see the magic !</b> "
+    INVALID_FORMAT = "<b>Error:</b>\n<b>I can only compress pdf documents. Please make it sure, you have given me a " \
+                     "valid document. Try again...</b>"
     WAIT_MESSAGE = "⌛️ <b>Processing</b> ⌛"
     DOWNLOAD_MSG = "⌛️ <b>Downloading</b> ⌛"
     UPLOAD_MSG = "⌛️ <b>Uploading</b> ⌛"
@@ -10,6 +9,6 @@ class Presets(object):
     UN_FINISHED_BAR = "◻️"
     FINISHED_DL = "<b>Success !</b>\n<i>Document downloaded successfully.</i>"
     START_COMPRESSING = "⌛️ <b>Processing</b>  ⌛\n<i>Trying to compress the document.</i>"
-    FINISHED_JOB = "<b>Success</b> ✅ \n\n<i>Size before job: {}\nSize after job: {}\n\nCredits: </i><a " \
-                   "href='https://github.com/m4mallu'><b> @M4Mallu</b></a> "
-    JOB_ERROR = "<b>Error:</b>\n<i>Something went wrong ! Process exited</i>"
+    FINISHED_JOB = "<b>Success</b> ✅ \n\n<i>Size before job: {}\nSize after job: {}\n\nBy: </b><a " \
+                   "href='https://t.me/tellybots_4u'><b>Tellybots_4u</b></a> "
+    JOB_ERROR = "<b>Error:</b>\n<b>Something went wrong ! Process exited</b>"
